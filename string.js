eec1b12fe191479f5995a6e2269bd9826a3b14a9
januar <januar@pop-os.localdomain>
2023-11-24 20:41:07 +0700

    string

---
+++ string.js
@@ -0,0 +1,8 @@
+const nama = "januarsyah akbar";
+console.info(nama.length);
+console.info(nama.toLowerCase());
+console.info(nama.toUpperCase());
+console.info(nama.split(" ")); // it will cahange to array with split the spasi
+
+const input = "    my name is akbar    ";
+console.info(input.trim()); // it will remove the spasi in the start and end
